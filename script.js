9a1ae6856a2903a3baaf3aff17e98bf8782e98e9
tsirdo8 <104384778+tsirdo8@users.noreply.github.com>
2025-05-31 00:58:57 +0400

    bug fix

--- script.js
+++ script.js
@@ -13,12 +13,15 @@ connectToDb();
 
 
 
-app.use(cors({
-    origin:  'http://localhost:5173',
-    credentials: true,  
+   app.use(cors({
+    origin: [
+        'http://localhost:5173', 
+       
+    ],
+    credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']  
-}));
+    allowedHeaders: ['Content-Type', 'Authorization']
+}))
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
