c8e4cf4819b70a9dabb002b9d17dd636d0bf31f6
tsirdo8 <104384778+tsirdo8@users.noreply.github.com>
2025-05-31 00:25:43 +0400

    bug fix

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ connectToDb();
 
 
 app.use(cors({
-    origin: 'https://mini-social-nhfz.vercel.app',
+    origin:  'http://localhost:5173',
     credentials: true,  
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']  
