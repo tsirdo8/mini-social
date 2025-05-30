d4d62ecf0da9b72d0b6aca7aeeee0a11697510db
tsirdo8 <104384778+tsirdo8@users.noreply.github.com>
2025-05-31 00:23:02 +0400

    bug fix

--- script.js
+++ script.js
@@ -11,10 +11,15 @@ const app = express();
 
 connectToDb();
 
+
+
 app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
-    credentials: true
+    origin: 'https://mini-social-nhfz.vercel.app' || 'http://localhost:5173',
+    credentials: true,  
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']  
 }));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('uploads'));
